659dbe30106a43588a69a37f7dd8dd31b8db6ba1
Olesea1986 <olesea86williams@gmail.com>
2020-12-24 11:53:35 -0600

    new group project start

---
+++ src/test/java/com/nextBaseCRM/test/olesika/Test1.java
@@ -0,0 +1,8 @@
+package com.nextBaseCRM.test.olesika;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Why is so hard to create a project with any commits?");
+
+    }
+}
