053703a9445e9e637af92c894b8f757346b8a0a1
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-24 11:57:40 -0600

    print 2

--- src/test/java/com/nextBaseCRM/test/Ahmed/HelloSelenium.java
+++ src/test/java/com/nextBaseCRM/test/Ahmed/HelloSelenium.java
@@ -3,6 +3,7 @@ package com.nextBaseCRM.test.Ahmed;
 public class HelloSelenium {
     public static void main(String[] args) {
         System.out.println("follow this");
+        System.out.println("then this");
 
     }
 }
