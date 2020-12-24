82c3f7e76abcfdf94b50b38a228fed2844cf3713
AzadBatch21 <sadikhov.azad@gmail.com>
2020-12-24 12:05:34 -0600

    added new steps

--- src/test/java/com/nextBaseCRM/test/Azad/Test1.java
+++ src/test/java/com/nextBaseCRM/test/Azad/Test1.java
@@ -1,8 +1,26 @@
 package com.nextBaseCRM.test.Azad;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello Group24");
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://login2.nextbasecrm.com/");
+        driver.findElement(By.name("USER_LOGIN")).sendKeys("helpdesk47@cybertekschool.com");
+        driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
+       // driver.findElement(By.name("type")).click();
+
+
+
     }
+//*[@id="login-popup"]
+
 }
 
