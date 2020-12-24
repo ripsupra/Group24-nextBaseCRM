d7b83dd9dd63a11cff2aafc78e42098f7715bfba
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-24 11:57:52 -0600

    print 3

--- src/test/java/com/nextBaseCRM/test/Ahmed/HelloSelenium.java
+++ src/test/java/com/nextBaseCRM/test/Ahmed/HelloSelenium.java
@@ -4,6 +4,7 @@ public class HelloSelenium {
     public static void main(String[] args) {
         System.out.println("follow this");
         System.out.println("then this");
+        System.out.println("and this ");
 
     }
 }
